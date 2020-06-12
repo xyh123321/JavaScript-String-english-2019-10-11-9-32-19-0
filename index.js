755ca2f51e3354bb8282c5a0831ceb3d98b152c1
jayden <376564838@qq.com>
2020-06-12 16:49:57 +0800

    commit codes

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
 // TODO write your code here...
+console.log('name：' + name.toUpperCase());
 
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
-
+var attr = sentence.split(" ");
+for (let i = 0; i < attr.length; i++) {
+    attr[i] = attr[i][0].toUpperCase() + attr[i].substring(1);
+}
+sentence = attr.join(" ");
+console.log('sentence：'+ sentence);
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
 //TODO write your code here...
+money = money.match(/\d+/g);
+console.log('money：'+money);
